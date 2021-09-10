44dbf03392618aa4647ab178f9acf8af2fc1a39c
Vera Kolotyuk <vmorozova77@gmail.com>
2021-09-10 20:23:05 +0300

    react imask added

--- src/components/AmountInput/AmountInput.js
+++ src/components/AmountInput/AmountInput.js
@@ -1,31 +1,26 @@
-import IMask from 'imask';
-import {useRef, useEffect} from 'react';
+import {IMaskInput} from 'react-imask';
 import './style.css';
 import {EXCEEDS_BALANCE_MESSAGE, EXCHANGE_INPUT_SCALE} from '../../utils/constants';
 
 const AmountInput = ({ value, onInputChangeHandler, showError }) => {
-    const input = useRef(null);
-    useEffect(() => {
-        IMask(input.current, {
-            mask: Number,
-            scale: EXCHANGE_INPUT_SCALE,
-            signed: true,
-            thousandsSeparator: '',
-            padFractionalZeros: false,
-            normalizeZeros: true,
-            radix: '.',
-            mapToRadix: ['.']
-        });
-    },[]);
-
+    const inputIMaskProps = {
+        mask: Number,
+        scale: EXCHANGE_INPUT_SCALE,
+        signed: true,
+        thousandsSeparator: '',
+        padFractionalZeros: false,
+        normalizeZeros: true,
+        radix: '.',
+        mapToRadix: ['.']
+    };
     return (
         <div>
-            <input
-                ref={input}
+            <IMaskInput
+                {...inputIMaskProps}
                 type="text"
-                value={value}
+                value={value.toString()}
                 onChange={onInputChangeHandler}
-                placeholder={"0"}
+                placeholder={'0'}
                 className="exchange-amount-input"
             />
             {showError && <div className="form-error">{EXCEEDS_BALANCE_MESSAGE}</div>}
